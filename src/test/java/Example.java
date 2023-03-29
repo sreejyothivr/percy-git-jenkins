fdf83c9357a7516ea7c32b107c058f788c2d6331
sreejyothi <sreejyothi@qburst.com>
2023-03-29 11:53:15 +0530

    parrallel

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,40 +1,43 @@
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 
 import io.percy.selenium.Percy;
 
-
-
 public class Example {
-	
+
+	WebDriver driver;
+	Percy percy;
+
+	@BeforeTest
+	public void setup() {
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(chromeOptions);
+		percy = new Percy(driver);
+
+	}
+
 	@Test
-	public void getstring()
-	{
-		
-		 ChromeOptions chromeOptions =new ChromeOptions();
-		    chromeOptions.addArguments("--remote-allow-origins=*");
-		    WebDriver driver =new ChromeDriver(chromeOptions);
-		    Percy percy = new Percy(driver);
-		    driver.get("https://www.saucedemo.com/");
-		    percy.snapshot("saucedemopage");
-	  }
-	
+	public void sauceDemoLaunch() {
+
+//		 ChromeOptions chromeOptions =new ChromeOptions();
+//		    chromeOptions.addArguments("--remote-allow-origins=*");
+
+		driver.get("https://www.saucedemo.com/");
+		percy.snapshot("saucedemopage");
+	}
+
 	@Test
-	public void getUrl()
-	{
-		
-		 ChromeOptions chromeOptions =new ChromeOptions();
-		    chromeOptions.addArguments("--remote-allow-origins=*");
-		    WebDriver driver =new ChromeDriver(chromeOptions);
-		    Percy percy = new Percy(driver);
-		    driver.get("https://www.tesla.com/");
-		    System.out.println(driver.getCurrentUrl());
-		    percy.snapshot("teslapage");
-	  }
-	
+	public void teslaLaunch() {
+
+		driver.get("https://www.tesla.com/");
+		System.out.println(driver.getCurrentUrl());
+		percy.snapshot("teslapage");
+	}
+
 }
