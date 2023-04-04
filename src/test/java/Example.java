595a09d33a0a316eb285995467b4db5c7661fa15
sreejyothi <sreejyothi@qburst.com>
2023-04-04 19:23:05 +0530

    parrallel-beforemethod-deleted

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -14,47 +15,53 @@ public class Example {
 	WebDriver driver;
 	Percy percy;
 
-	@Parameters("Browser")
-	@BeforeTest(alwaysRun = true)
-	
-	public void setup(String browser) {
-		if (browser.equals("edge")) {
-//		ChromeOptions chromeOptions = new ChromeOptions();
-//		chromeOptions.addArguments("--remote-allow-origins=*");
-			driver=new EdgeDriver();
-		//driver = new ChromeDriver(chromeOptions);
-		
-		}
-		
-		else if(browser.equals("chrome"))
-	{
-			//driver=new ChromeDriver();
+	//@Parameters("Browser")
+//	@BeforeMethod(alwaysRun = true)
+//	
+//	public void setup(String browser) {
+//		if (browser.equals("edge")) {
+////		ChromeOptions chromeOptions = new ChromeOptions();
+////		chromeOptions.addArguments("--remote-allow-origins=*");
+//			
+//		//driver = new ChromeDriver(chromeOptions);
+//		
+//		}
+//		
+//		else if(browser.equals("chrome"))
+//	{
+//			//driver=new ChromeDriver();
+////		percy = new Percy(driver);
+//			
+////			
+////			ChromeOptions chromeOptions = new ChromeOptions();
+////		chromeOptions.addArguments("--remote-allow-origins=*");
+////		driver = new ChromeDriver(chromeOptions);
+//			
+//		}
 //		percy = new Percy(driver);
-			ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--remote-allow-origins=*");
-		driver = new ChromeDriver(chromeOptions);
-			
-		}
-		percy = new Percy(driver);
-	}
+//	}
 
 	@Test
 	public void sauceDemoLaunch() {
 
 //		 ChromeOptions chromeOptions =new ChromeOptions();
 //		    chromeOptions.addArguments("--remote-allow-origins=*");
-
-		driver.get("https://www.saucedemo.com/");
+		driver=new EdgeDriver();
+		percy = new Percy(driver);
+		driver.get("https://saucelabs.com/");
 		percy.snapshot("saucedemopage");
 		
 	}
 
 	@Test
 	public void teslaLaunch() {
-
-		driver.get("https://www.google.com/");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(chromeOptions);
+		percy = new Percy(driver);
+		driver.get("https://www.qburst.com/");
 		System.out.println(driver.getCurrentUrl());
-		percy.snapshot("google page");
+		percy.snapshot("Qburst page");
 	}
 
 }
